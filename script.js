b414e591bceb0fc32a9b5216de5bab8da3a9ab86
semapavel <semapavel89@gmail.com>
2022-06-21 17:59:34 +0500

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let age = prompt('Введите возраст:', '');
-
-if (age < 14 || age>90) {
-	alert(true);
+let name = prompt("Кто там?", '');
+if (name === 'Админ') {
+	let password = prompt('Пароль?', '');
+	if (password === 'Я главный') {
+		alert( 'Здравствуйте!' );
+	} else if (password === '' || password === null) {
+		alert( 'Отменено' );
+	} else {
+		alert( 'Неверный пароль' );
+	}
+} else if (name === '' || name === null) {
+	alert( 'Отменено' );
 } else {
-	alert(false);
+	alert( "Я вас не знаю" );
 }
